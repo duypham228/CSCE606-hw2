6bd42f16ec7d3c38eec21bfbe6596285d5b59d58
EC2 Default User <ec2-user@ip-172-31-9-15.us-east-2.compute.internal>
2021-02-24 23:27:27 +0000

    add form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,7 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = ['G','PG','PG-13','R']
-    @ratings_to_show = []
+    @ratings_to_show = ratings
     @movies = Movie.all
     
   end
