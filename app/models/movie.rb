141b596ff4b292d61121f1f97a2bafe8c98e5f46
EC2 Default User <ec2-user@ip-172-31-9-15.us-east-2.compute.internal>
2021-02-25 07:38:34 +0000

    remember both filter and ratings params debug

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,11 +2,19 @@ class Movie < ActiveRecord::Base
     def self.with_ratings(ratings_list, sort_by)
         # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
         if !ratings_list.empty?
-            return self.where(rating: ratings_list)
+            if sort_by.empty?
+                return self.where(rating: ratings_list)
+            else
+                return self.where(rating: ratings_list).order(sort_by)
+            end
         #  movies with those ratings
         # if ratings_list is nil, retrieve ALL movies
         else
-            return self.all
+            if sort_by.empty?
+                return self.all
+            else
+                return self.all.order(sort_by)
+            end
         end
     end
     
