5dfdf246a3c407c8be99899e7c6c2812a94ce1a1
EC2 Default User <ec2-user@ip-172-31-9-15.us-east-2.compute.internal>
2021-02-25 01:39:40 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Movie.all_ratings
+    @all_ratings = ['G','PG','PG-13','R']
     
     if params[:ratings]
       @ratings_to_show = params[:ratings].keys
