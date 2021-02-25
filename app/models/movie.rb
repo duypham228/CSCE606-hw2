d37ad8610f5889f2775d75ebe021b4f5077c953f
EC2 Default User <ec2-user@ip-172-31-9-15.us-east-2.compute.internal>
2021-02-25 00:43:56 +0000

    add form

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,12 @@
 class Movie < ActiveRecord::Base
     def self.with_ratings(ratings_list)
         # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+        list = []
         if !ratings_list.empty?
             ratings_list.each do |rating|
-               where("rating = '#{rating}'") 
+               list.concat(where("rating = '#{rating}'")) 
             end
+            return list
         #  movies with those ratings
         # if ratings_list is nil, retrieve ALL movies
         else
